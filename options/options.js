0226a09aadd540e167ad5353c413043a44ff7ddd
Crowdbotics <team@crowdbotics.com>
2021-08-07 19:39:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bitter_snowflake_29620Identifier",
+  url: "https://bitter_snowflake_29620Identifier.botics.co",
+  api: "https://bitter_snowflake_29620Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
